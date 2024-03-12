04a577f3482abbe778f6def78ac89e96fe5d66ed
SunPraise <xenothestrange@yahoo.com>
2024-03-11 21:56:16 -0800

    Removed the usage of CompFlickable. Added textures for rail switch direction
    Previously the Rail Switch used CompFlickable to do it's stuff. That comp handled the UI gizmo, flicking, telling pawns to go flick the switch, and more.
    Now it's handled by a WorkGiver.
    
    The only noticeable change in gameplay is that a switch waiting to be flicked will not have the usual icon above it to say as much... because I couldn't figure out how to use Designations like I should.
    
    ex. Map.designationManager.DesignationOn(this, DesignationDefOf.FlickRailSwitch)
    
    I really should learn how to use comps and make a custom comp for the rail switch which can use its own designators or whatever.
    
    Reverted  changelog

--- Source/Minecart/JobDefOf.cs
+++ Source/Minecart/JobDefOf.cs
@@ -7,4 +7,9 @@ namespace Minecart;
 public static class JobDefOf
 {
     public static JobDef LaunchCart;
+    public static JobDef Job_SwitchRailDirection;
+}
+public static class WorkGiverDefOf
+{
+    public static WorkGiver_Scanner WorkGiver_SwitchRailDirection;
 }
